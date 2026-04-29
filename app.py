f4361b6efaec04e82b4c41e8d7c53c8450d43a90
dhruv9519sri <dhruv9519srivastava@gmail.com>
2026-04-29 13:03:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="CLF Assessment", layout="wide")
 
-st.title("CLF Sustainability Assessment Tool")
+st.title("CLF Sustainability Assessment Tool / सीएलएफ स्थिरता मूल्यांकन उपकरण")
 
-st.write("Welcome to the REAP CLF Sustainability Assessment")
+st.write("Welcome to the REAP CLF Sustainability Assessment / REAP सीएलएफ स्थिरता मूल्यांकन में आपका स्वागत है")
 
-st.page_link("pages/Survey.py", label="Start Survey")
-st.page_link("pages/Admin_Login.py", label="Admin Login")
+st.page_link("pages/Survey.py", label="Start Survey / सर्वे शुरू करें")
+st.page_link("pages/Admin_Login.py", label="Admin Login / प्रशासक लॉगिन")
