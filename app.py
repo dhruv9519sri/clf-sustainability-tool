8b322bd91aba99828390cbecb305675e29514a79
dhruv9519sri <dhruv9519srivastava@gmail.com>
2026-04-29 13:05:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="CLF Assessment", layout="wide")
 
-st.title("CLF Sustainability Assessment Tool / सीएलएफ स्थिरता मूल्यांकन उपकरण")
+st.title("CLF Sustainability Assessment Tool\nसीएलएफ स्थिरता मूल्यांकन उपकरण")
 
 st.write("Welcome to the REAP CLF Sustainability Assessment / REAP सीएलएफ स्थिरता मूल्यांकन में आपका स्वागत है")
 
